656efac76946f7889a05da08d40562cc43cb84f4
Aditya Verma <73935799+homewardgamer@users.noreply.github.com>
2021-09-13 13:54:08 +0530

    Add files via upload

---
+++ hall.js
@@ -0,0 +1,23 @@
+getData();
+var data;
+async function getData() {
+
+    const response = await fetch('HOF.csv');
+    data = await response.text();
+    console.log(data);
+    const rows = data.split('\n').slice(1);
+    const loop = rows.forEach(row => {
+        const elt = row.split(',');
+            var element = " <a class=\"carousel-item-image\" href=\"#one!\"><img src = \"Rectangle 17.png\" /></a> <h5>" +
+                elt[0] + "</h5> <h6>" + elt[2] + " Wing" + " '" + elt[1].substring(2) + "</h6>";
+            var elem = document.createElement("div");
+            elem.className = "carousel-item";
+            elem.innerHTML = element;
+            document.getElementById(elt[1]).appendChild(elem);
+    });
+}
+setTimeout(() => {
+    $(document).ready(function () {
+        $(".carousel").carousel();
+    });
+}, 500);
