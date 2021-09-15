7c7608706ad26569d28ee673910dae8fb5df0655
Aditya Verma <73935799+homewardgamer@users.noreply.github.com>
2021-09-15 16:21:50 +0530

    Add files via upload

--- hall.js
+++ hall.js
@@ -1,4 +1,5 @@
-getData();
+$(document).ready(getData);
+
 var data;
 async function getData() {
 
@@ -6,18 +7,19 @@ async function getData() {
     data = await response.text();
     console.log(data);
     const rows = data.split('\n').slice(1);
-    const loop = rows.forEach((row,index) => {
+    const loop = rows.forEach((row, index) => {
         const elt = row.split(',');
-            var element = " <a class=\"carousel-item-image\" href=\"alumniinfo.html?key="+index+"\"><img src ='assets/"+elt[3]+"'/></a> <h5>" +
-                elt[0] + "</h5> <h6>" + elt[2] + " Wing" + " '" + elt[1].substring(2) + "</h6>";
-            var elem = document.createElement("div");
-            elem.className = "carousel-item";
-            elem.innerHTML = element;
-            document.getElementById(elt[1]).appendChild(elem);
+        var element = " <a class=\"carousel-item-image\" href=\"alumniinfo.html?key=" + index + "\"><img src ='assets/" + elt[3] + "'/></a> <h5>" +
+            elt[0] + "</h5> <h6>" + elt[2] + " Wing" + " '" + elt[1].substring(2) + "</h6>";
+        var elem = document.createElement("div");
+        elem.className = "carousel-item";
+        elem.innerHTML = element;
+
+        document.getElementById(elt[1]).appendChild(elem);
     });
-}
-setTimeout(() => {
-    $(document).ready(function () {
+    setTimeout(() => {
         $(".carousel").carousel();
-    });
-}, 500);
+    }, 600);
+    
+
+}
