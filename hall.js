187482132d2ca691de736dfcc65af68e408bb046
Aditya Verma <73935799+homewardgamer@users.noreply.github.com>
2021-09-18 10:49:36 +0530

    Add files via upload

--- hall.js
+++ hall.js
@@ -10,26 +10,27 @@ async function getData() {
 
     data = await response.text();
     const rows = data.split('\n').slice(1);
+    rows.pop();
     const loop = rows.forEach((row, index) => {
         const elt = row.split(',');
-        var element = " <a class=\"carousel-item-image\" href=\"alumniinfo.html?key="+index+"\"><img src ='assets/"+elt[3]+"' height=300px width=200px/></a> <h5>" +
-                elt[0] + "</h5> <h6>" + elt[2] + " Wing" + " '" + elt[1].substring(2) + "</h6>";
+        var element = " <a class=\"carousel-item-image\" href=\"alumniinfo.html?key=" + index + "\"><img src ='assets/" + elt[3] + "' height=300px width=200px/></a> <h5>" +
+            elt[0] + "</h5> <h6>" + elt[2] + " Wing" + " '" + elt[1].substring(2) + "</h6>";
         var elem = document.createElement("div");
         elem.className = "carousel-item";
         elem.innerHTML = element;
-
         document.getElementById(elt[1]).appendChild(elem);
+
     });
-    setTimeout(() => {
-        $(".carousel").carousel({
-            duration: 200,
-        });
-        autoplay();
-    function autoplay(){
+    await loop;
+    $(".carousel").carousel({
+        duration: 200,
+    });
+    autoplay();
+
+    function autoplay() {
         $('.carousel').carousel('next');
-        setTimeout(autoplay,2000);
+        setTimeout(autoplay, 2000);
     }
-    }, 600);
 
 
 }
